6d73fd600e16001658d182e85fbdcaea6291046d
dmd <159908622+dmd03@users.noreply.github.com>
2024-05-08 20:19:55 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import About from "./components/AboutComponent.vue";
+import Calendly from "./components/CalendlyComponent.vue";
+import Footer from "./components/FooterComponent.vue";
+import Header from "./components/HeaderComponent.vue";
+import Hero from "./components/HeroComponent.vue";
+import Testimonial from "./components/TestimonialComponent.vue";
+</script>
+
+<template>
+  <div class="w-full h-auto bg-black pt-[35px]">
+    <!-- <div class="sm:px-4 md:px-6 lg:px-[35px] xl:px-[0px]"> -->
+    <div class="px-[35px] lg:px-[35px] xl:px-[0px]">
+      <Header />
+      <Hero />
+      <About />
+      <Calendly />
+      <Testimonial />
+    </div>
+    <Footer />
+  </div>
+</template>
---
+++ src/components/AboutComponent.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="max-w-[1140px] m-auto">
+    <div class="flex flex-col-reverse items-center xl:flex-row">
+      <img src="../assets/img/about.png" alt="logo" class="" />
+
+      <div
+        class="flex flex-col justify-center items-center text-center xl:justify-end xl:items-end xl:text-right pl-0 lg:pl-0 xl:pl-5"
+      >
+        <h3
+          class="text-white text-[32px] sm:text-[32px] md:text-[56px] lg:text-[56px] xl:text-[56px] font-syne font-bold leading-none w-full xl:w-[516px]"
+        >
+          Cine suntem noi?
+        </h3>
+
+        <p
+          class="text-silver font-rubik text-base font-normal leading-[154.5%] tracking-[0.99px]"
+        >
+          Aaronn is a New York-based visual designer focusing on branding and
+          visual identity. Her portfolio showcases her wide range of work,
+          spanning books, posters and web design.
+        </p>
+
+        <a href="#" class="mt-[30px] mb-[32px] xl:mb-0">
+          <button
+            class="flex px-[50px] py-[20px] gap-[10px] text-white text-center font-rubik text-[16px] font-semibold tracking-wider rounded-full bg-[#213B2A]"
+          >
+            Afla mai multe
+          </button>
+        </a>
+      </div>
+    </div>
+
+    <div class="py-[164px] flex flex-col items-center xl:flex-row">
+      <div
+        class="flex flex-col justify-center items-center text-center xl:justify-start xl:items-start xl:text-left pr-0 lg:pr-0 xl:pr-5"
+      >
+        <h3
+          class="text-white text-[32px] sm:text-[32px] md:text-[56px] lg:text-[56px] xl:text-[56px] font-syne font-bold leading-none w-full xl:w-[516px]"
+        >
+          Inscrieri Olimpiada <span class="text-yellow">AC</span> 2024
+        </h3>
+
+        <p
+          class="text-silver font-rubik text-base font-normal leading-[154.5%] tracking-[0.99px]"
+        >
+          Aaronn is a New York-based visual designer focusing on branding and
+          visual identity. Her portfolio showcases her wide range of work,
+          spanning books, posters and web design.
+        </p>
+
+        <a href="#" class="mt-[30px] mb-[32px] xl:mb-0">
+          <button
+            class="flex px-[50px] py-[20px] gap-[10px] text-white text-center font-rubik text-[16px] font-semibold tracking-wider rounded-full bg-[#213B2A]"
+          >
+            INSCRIE-TE ACUM!
+          </button>
+        </a>
+      </div>
+
+      <img src="../assets/img/about-1.png" alt="logo" class="" />
+    </div>
+  </div>
+</template>
